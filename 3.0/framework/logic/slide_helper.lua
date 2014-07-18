6a046da015e298fe3b99968ad2a6fb9943a719cf
yestein <yestein86@gmail.com>
2014-07-18 15:46:01 +0800

    增加滑动拖拽功能，拆分CD模块

--- 3.0/framework/logic/slide_helper.lua
+++ 3.0/framework/logic/slide_helper.lua
@@ -13,9 +13,14 @@ function SlideHelper:_Unnit()
 	self.timer_id = nil
 	self.is_working = nil
 	self.container = nil
+	self.offset_y = nil
+	self.offset_x = nil
 end
 
-function SlideHelper:_Init()
+function SlideHelper:_Init(is_drag, offset_x, offset_y)
+	self.is_drag = is_drag or 0
+	self.offset_x = offset_x or 0
+	self.offset_y = offset_y or 0
 	self.container = {}
 	self.is_working = 0
 	self.timer_id = nil
@@ -45,13 +50,15 @@ function SlideHelper:End()
 		RealTimer:CloseTimer(self.timer_id)
 		self.timer_id = nil
 	end
-	Event:FireEvent("SLIDE.END", self, self.last_x, self.last_y)
+	local end_x = self.last_x and self.last_x + self.offset_x
+	local end_y = self.last_y and self.last_y + self.offset_y
+	Event:FireEvent("SLIDE.END", self, end_x, end_y)
 	self:Clear()
 	self.is_working = 0
 end
 
 function SlideHelper:AddSprite(id, sprite)
-	self.container[id] = sprite
+	self.container[id] = 1
 end
 
 function SlideHelper:RemoveSprite(id, sprite)
@@ -60,16 +67,13 @@ end
 
 function SlideHelper:OnMove(x, y)
 	if self.last_x and self.last_y then
-		local move_x = x - self.last_x
-		local move_y = y - self.last_y
-		for id, sprite in pairs(self.container) do
-			local raw_x, raw_y = sprite:getPosition()
-			--local new_x, new_y = raw_x + move_x, raw_y + move_y
-			local new_x, new_y = x, y - 60
-			sprite:setPosition(new_x, new_y)
-			Event:FireEvent("SLIDE.MOVE", id, new_x, new_y)
+		if self.is_drag == 1 then
+			for id, _ in pairs(self.container) do
+				Event:FireEvent("SLIDE.MOVE_OBJ", id, x + self.offset_x, y + self.offset_y)
+			end
 		end
 	end
+	Event:FireEvent("SLIDE.MOVE", self.last_x, self.last_y, x, y)
 	self.last_x = x
 	self.last_y = y
 end
