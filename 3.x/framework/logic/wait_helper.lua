c2b67fe8707f6af5c7e0a5058fd0b1046f340061
yestein <yestein86@gmail.com>
2014-09-01 11:45:18 +0800

    Improve WaitHelper

--- 3.x/framework/logic/wait_helper.lua
+++ 3.x/framework/logic/wait_helper.lua
@@ -14,29 +14,26 @@ WaitHelper.is_debug = 1
 
 function WaitHelper:_Uninit()
 	self.next_job_id        = nil
-	self.func_timer_over    = nil	
 	self.complete_call_back = nil
-	self.max_wait_frame     = nil
 	self.job_count          = nil
 	self.job_list           = nil
 
 	return 1
 end
 
-function WaitHelper:_Init(max_wait_time, complete_call_back, func_timer_over)
+function WaitHelper:_Init(complete_call_back)
 	self.job_list = {}
 	self.job_count = 0
-	self.max_wait_frame = max_wait_time * GameMgr:GetFPS()
 	self.complete_call_back = complete_call_back
-	self.func_timer_over = func_timer_over
 	self.next_job_id = 1
 	return 1
 end
 
-function WaitHelper:WaitJob()
+function WaitHelper:WaitJob(max_wait_time, func_over_time)
 	local job_id = self.next_job_id
 	self.next_job_id = self.next_job_id + 1
-	self.job_list[job_id] = self:RegistRealTimer(self.max_wait_frame, {self.OnTimeOver, self, job_id})
+	self.job_list[job_id] = {func_over_time}
+	self:RegistRealTimer(max_wait_time * GameMgr:GetFPS(), {self.OnTimeOver, self, job_id})
 	self.job_count = self.job_count + 1
 	if self.is_debug == 1 then
 		print("wait job", job_id)
@@ -46,8 +43,9 @@ function WaitHelper:WaitJob()
 end
 
 function WaitHelper:OnTimeOver(job_id, timer_id)
-	if self.func_timer_over then
-		self.func_timer_over(job_id)
+	local func_timer_over = self.job_list[job_id][1]
+	if func_timer_over then
+		func_timer_over(job_id)
 	end
 	self:JobComplete(job_id)
 end
